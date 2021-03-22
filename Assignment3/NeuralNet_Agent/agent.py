f52dda38212232a0ed866850ad1dfb9fe9202e92
Pawan <pbilkhu15@gmail.com>
2021-03-22 16:29:19 -0700

    In agent.py the files that are read in are closed after use, renamed a method for better readability. Additionally a unittest was made for all the major methods in the Agent class, readme was updated to reflect these changes

--- Assignment3/NeuralNet_Agent/agent.py
+++ Assignment3/NeuralNet_Agent/agent.py
@@ -28,7 +28,7 @@ class Agent:
             model (object): An object containing a trained model.
 
         Methods:
-            constructSentence(): deconstructs sentences into their root words.
+            deconstructSentence(): deconstructs sentences into their root words.
             bagWords(): uses the deconstructed sentence to a series of words and maps it to a matching tag.
             predictResponse(): uses the chatbot model to return a response, with an associated probability.
             getResponse(): returns random bot response that has a greater probability than the minimum threshold.
@@ -40,13 +40,19 @@ class Agent:
         # Create a lemmatizer object
         self.lemmatizer = WordNetLemmatizer()
         # read in intents.json file
-        self.intents = json.loads(open('intents.json').read())
+        file = open('intents.json')
+        self.intents = json.loads(file.read())
+        file.close()
         # load in the tags, and responses from the pickle files and load the saved model
-        self.tags = pickle.load(open('tags.pk1', 'rb'))
-        self.responses = pickle.load(open('responses.pk1', 'rb'))
+        file = open('tags.pk1', 'rb')
+        self.tags = pickle.load(file)
+        file.close()
+        file = open('responses.pk1', 'rb')
+        self.responses = pickle.load(file)
+        file.close()
         self.model = load_model('chatbotmodel.h5')
 
-    def constructSentence(self, sentence):
+    def deconstructSentence(self, sentence):
         """
         This is a methods takes sentence and deconstructs it into its words, and breaks
         each word into it's stem word.
@@ -61,7 +67,7 @@ class Agent:
 
     def bagWords(self, sentence):
         """
-        This is a methods takes sentence and uses the constructSentence methods and creates a
+        This is a methods takes sentence and uses the deconstructSentence methods and creates a
         bag of words (the same length as the tags), that is, it constructs an array with zeros everywhere
         except where the tags matches a word from the sentence.
         Parameters:
@@ -69,7 +75,7 @@ class Agent:
         Returns:
             bag (numpy array): an numpy array for the model
         """
-        separatedWords = self.constructSentence(sentence)
+        separatedWords = self.deconstructSentence(sentence)
         bag = [0] * len(self.tags)
 
         # each word in the list
@@ -80,7 +86,6 @@ class Agent:
                 # if a key matches a word, set the bag at the given index to 1
                 if key == word:
                     bag[i] = 1
-
         return np.array(bag)
 
     def predictResponse(self, sentence):
@@ -141,7 +146,7 @@ class Agent:
 # run the chat bot
 def main():
     chatBot = Agent()
-    chatBot.run()
+    print(chatBot.getResponse(chatBot.predictResponse("What hours are you open?")))
 
 
 if __name__ == '__main__':
